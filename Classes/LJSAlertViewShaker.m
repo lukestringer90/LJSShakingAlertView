3551602195406f8bfa033f89a445e2e203f502b4
Luke Stringer <lukestringer630@gmail.com>
2013-12-28 21:53:56 +0000

    Naive shaking animation using affine transforms.

--- Classes/LJSAlertViewShaker.m
+++ Classes/LJSAlertViewShaker.m
@@ -7,10 +7,34 @@
 //
 
 #import "LJSAlertViewShaker.h"
+#import "LJSShakingAlertView.h"
 
 @implementation LJSAlertViewShaker
 
 - (void)shakeAlertView:(LJSShakingAlertView *)alertView {
+    
+    CGAffineTransform moveRight = CGAffineTransformTranslate(CGAffineTransformIdentity, 20, 0);
+    CGAffineTransform moveLeft = CGAffineTransformTranslate(CGAffineTransformIdentity, -20, 0);
+    CGAffineTransform resetTransform = CGAffineTransformTranslate(CGAffineTransformIdentity, 0, 0);
+    
+    [UIView animateWithDuration:0.1 animations:^{
+        alertView.transform = moveLeft;
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.1 animations:^{
+            alertView.transform = moveRight;
+        } completion:^(BOOL finished) {
+            [UIView animateWithDuration:0.1 animations:^{
+                alertView.transform = moveLeft;
+                
+            } completion:^(BOOL finished) {
+                [UIView animateWithDuration:0.1 animations:^{
+                    alertView.transform = resetTransform;
+                }];
+            }];
+            
+        }];
+    }];
+    
 }
 
 @end
